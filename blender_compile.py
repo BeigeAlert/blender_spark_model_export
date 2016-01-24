de6e9745e2392b7553a3098a5a77c0e0ad18633b
Trevor Harris <trevorharrisvfx@gmail.com>
2016-01-23 21:31:50 -0600

    This file has a new home! :)

---
+++ blender_compile.py
@@ -0,0 +1,13 @@
+import sys
+import bpy
+
+for i,v in enumerate(sys.argv):
+    if v == '--':
+        if len(sys.argv) <= i+1:
+            exit(1)
+        sys.path.insert(0, sys.argv[i+1])
+        break
+import export_spark_model
+export_spark_model.save()
+
+bpy.ops.wm.quit_blender()
